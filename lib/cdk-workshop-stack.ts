8b1a35607faf4caa9f29b95ec114ca65fe07aff8
Nikolay Prieto <enprietop@unal.edu.co>
2022-10-03 19:16:24 -0500

    Advances in workshop, dynamo DB table

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,19 +1,17 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
-
-export class CdkWorkshopStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
-
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
-  }
+import * as cdk from 'aws-cdk-lib'
+import * as apigw from 'aws-cdk-lib/aws-apigateway'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+export class CdkWorkshopStack extends cdk.Stack {
+  constructor(scope: cdk.App, id:string, props?:
+    cdk.StackProps) {
+      super(scope, id, props);
+      // defines an AWS lambda resource
+      const hello = new lambda.Function(this, 'HelloHandler', {
+        runtime: lambda.Runtime.NODEJS_14_X, // Execution env
+        code: lambda.Code.fromAsset('lambda'), // Loaded from directory called lambda
+        handler: 'hello.handler'
+      });
+      // Defining API gateway REST API resource 
+      new apigw.LambdaRestApi(this, "Endpoint", {handler: hello});
+    }
 }
